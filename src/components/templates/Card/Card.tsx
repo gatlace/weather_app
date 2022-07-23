1f0a3eb8a2a74a865b24419fea7520f1f6c422ea
gatlace <zii.donato@outlook.com>
2022-07-23 11:45:41 +0000

    fixed desktop forecast

--- src/components/templates/Card/Card.tsx
+++ src/components/templates/Card/Card.tsx
@@ -6,7 +6,7 @@ const Card = (props: PropsWithChildren) => {
   return (
     <div className={`flex flex-col p-2 rounded-lg 
                     border-2 border-white
-                  overflow-hidden h-full min-w-max max-h-80 `}>
+                  overflow-hidden h-full min-w-max max-h-80 w-full `}>
       {props.children}
     </div>
     
